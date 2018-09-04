78f733dc3208c611501c573a9533ac4ad1f53d8f
Chuong Vu <chuongvu@Chuongs-MacBook-Pro.local>
2018-09-04 14:42:08 -0500

    Added roll function to the dice class

--- src/main/java/JCK/Risk/Gameplay/Dice.java
+++ src/main/java/JCK/Risk/Gameplay/Dice.java
@@ -1,5 +1,11 @@
 package JCK.Risk.Gameplay;
+import java.util.Random;
 
 public class Dice {
-
+	private int diceValue = 0;
+	public int roll() {
+		Random random = new Random();
+		diceValue = random.nextInt(6) + 1;
+		return diceValue;
+	}
 }
