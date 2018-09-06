59612f792d4df031e0408a1c0e280d784d8bdad0
Jeffry Sandoval <jeffrysandoval24@gmail.com>
2018-09-05 23:31:03 -0500

    Added Card class & initialized them in Start

--- src/main/java/JCK/Risk/Gameplay/Game.java
+++ src/main/java/JCK/Risk/Gameplay/Game.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Random;
 
 import JCK.Risk.Players.Player;
 
@@ -13,16 +16,27 @@ public class Game {
 	
 	//this is the array list that will be used for the turns
 	//TODO: this should be in the class of turns
-	ArrayList<Player> circularPlayersArray = new ArrayList<Player>();
+	ArrayList<Player> playersArray = new ArrayList<Player>();
+	
+
 	
 	public void initializeGame(int numPlayers) throws IOException 
 	{
 		
 		this.numberOfPlayers = numPlayers;
 		
+		
+
+		//XXX this is what we're gonna use for the die values
+		//Random die1 = new Random(numPlayers);
+		//System.out.println(die1.nextInt(6));
+
+		Random randNumber = new Random();
+		
 		//this loop will initialize the players
 		for(int i = 0; i < numPlayers; i++)
 		{
+			
 			
 			
 			
@@ -32,24 +46,65 @@ public class Game {
 			String playerName = br.readLine();
 			
 			
-			
-			//TODO: here we need to roll the dice
-			//int rollValue = Dice.getTurns();
-			int rollValue = 0;
-			System.out.println("Your turn number is");
-			
-			
+			int turnValue = randNumber.nextInt(1000); //random number from 0 to 999
+
+
 			
 			
 			Player newPlayer = new Player();
 			
-			newPlayer.createPlayer(playerName, rollValue);
+			newPlayer.createPlayer(playerName, turnValue);
+			
+			
+			playersArray.add(newPlayer);
+			
+			//add this new player to the array
+			//then we're going to sort the array based on that value
+			
+			
+			
+		}
+		
+		//sort the array here by the turnValue
+		Collections.sort(playersArray, Game.COMPARE_BY_VALUE);
+		
+		
+		
+		
+		System.out.println("The following is turn order:");
+		
+		//TODO do we need to set the roll value like below?
+		for(int i = 0; i < playersArray.size(); i++)
+		{
+			playersArray.get(i).rollValue = i + 1; //sets the roll value if we need to set it
+			
+			//but as of now a circular array type of setting for turns is enough
+			System.out.println((i + 1) + ". " + playersArray.get(i).name);
 			
 		}
 		
 		
 	}
 	
+	
+	
+	public ArrayList<Player> getPlayersArray() {
+		
+		return this.playersArray;
+	}
+	
+	
+	
+	public static Comparator<Player> COMPARE_BY_VALUE = new Comparator<Player>() {
+		public int compare(Player one, Player other) {
+			
+			
+			
+			return one.turnValue.compareTo(other.turnValue);
+		}
+		
+	};
+		
 	
 	
 	
--- src/main/java/JCK/Risk/Start.java
+++ src/main/java/JCK/Risk/Start.java
@@ -3,24 +3,37 @@ package JCK.Risk;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
+import JCK.Risk.Gameplay.Card;
 import JCK.Risk.Gameplay.Game;
+import JCK.Risk.Players.Player;
 
 public class Start {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-
+		
+		
 		
 		System.out.println("Hello, and welcome to Risk!");
 		System.out.println("How many players will be playing today?");
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		//TODO, get the bufferedreader to only accept values from 1 to 6
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int numPlayers = Integer.parseInt(br.readLine());
 		
 		Game startGame = new Game();
+		Card cards = new Card();
 		
 		startGame.initializeGame(numPlayers);
+		ArrayList<Player> playersArray = startGame.getPlayersArray();
+		
+		
+		cards.initializeCards();
+		
+		//initialize continents
+		//initialize territories
+		
 	}
 
 }
