0c9c995740b65b1ae9a97bdf3175b8fd4a7c0be1
Chuong Vu <chuongvu@Chuongs-MacBook-Pro.local>
2018-09-29 23:41:57 -0500

    Worked on and finished issue of getting new soldiers based on the amount of territories owned as well as the continents owned. Also finished the issue of assigning these new soldiers to territories as well.

--- src/main/java/JCK/Risk/Gameplay/Turns.java
+++ src/main/java/JCK/Risk/Gameplay/Turns.java
@@ -1,19 +1,106 @@
 package JCK.Risk.Gameplay;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
 import JCK.Risk.Locations.Continent;
 import JCK.Risk.Locations.Territory;
+import JCK.Risk.Players.Player;
 
 public class Turns {
-	
-	public void initialSetup(int numPlayers)
-	{
-		
-		
+	public Turns(Game game) {
+		int playerTurnCount = 0;
+		while (game.getPlayersArray().size() != 1) {
+			Player player = game.getPlayersArray().get(playerTurnCount);
+			System.out.println(playerTurnCount);
+			System.out.println(player.getName());
+			getNewSoldiers(player, game.getContinentArray());
+			placeNewSoldiers(game, player);
+			playerTurnCount++;
+			playerTurnCount %= game.getPlayersArray().size();
+		}
 	}
 	
+	/**
+	 * Method used to get the amount of soldiers that the player gets every turns based on
+	 * the amount of territories and continents the player owns
+	 * @param player
+	 * @param continentArray
+	 */
+	public void getNewSoldiers(Player player, List<Continent> continentArray) {
+		player.setSoldiersToPlace(player.getSoldiersToPlace() + (int) player.getTerritoriesOwned().size()/3);
+		for (int i = 0; i < continentArray.size(); i++) {
+			if (continentArray.get(i).playerOwnsContinent(player)) {
+				player.setSoldiersToPlace(player.getSoldiersToPlace() + continentArray.get(i).getContinentValue());
+			}
+		}
+	}
+	/**
+	 * Places the soldiers that the player has onto territories that the player owns
+	 * @param game
+	 * @param player
+	 */
+	public void placeNewSoldiers(Game game, Player player) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		List<Continent> continentArray = game.getContinentArray();
+		while (player.getSoldiersToPlace() > 0) {
+			try {
+				// asks the user which territory to place soldier onto
+				game.displayWorld();
+				System.out.println(player.getName() + ", you have " + player.getSoldiersToPlace() + " soldiers to place. Choose a territory you own to place: ");
+				String userInput = br.readLine();
+				while (!playerOwnsTerritory(userInput, player, continentArray)) {
+					System.out.println("Please enter a territory you own: ");
+					userInput = br.readLine();
+				}
+				
+				// asks the user how many soldier to place on the territory defined in the variable userInput
+				System.out.println("How many soldier do you want to place on " + userInput + "?");
+				int soldiersToPlace = Integer.parseInt(br.readLine());
+				while (soldiersToPlace > player.getSoldiersToPlace() || soldiersToPlace < 0) {
+					System.out.println("Enter an amount less than or equal to the amount you have to place.");
+					soldiersToPlace = Integer.parseInt(br.readLine());
+				}
+				
+				// places the amount of soldiers onto the territory the user defines
+				for (int i = 0; i < continentArray.size(); i++) {
+					if (continentArray.get(i).getTerritory(userInput) != null) {
+						HashMap<String, Territory> listOfTerritories = continentArray.get(i).getListOfTerritories();
+						listOfTerritories.put(userInput, listOfTerritories.get(userInput).addSoldiers(soldiersToPlace));
+					}
+				}
+				
+				// subtracts the number of soldiers the player can place by the number of soldiers just placed
+				player.setSoldiersToPlace(player.getSoldiersToPlace() - soldiersToPlace);
+			} catch (IOException e) {
+				System.out.println("You have entered an invalid input. Please try again.");
+			}
+		}
+	}
+	
+	/**
+	 * Checks if the player owns all of the territories in the continen
+	 * @param userInput
+	 * @param player
+	 * @param game
+	 * @return
+	 */
+	public boolean playerOwnsTerritory(String userInput, Player player, List<Continent> continentArray) {
+		for (int i = 0; i < continentArray.size(); i++) {
+			if (continentArray.get(i).getTerritory(userInput) != null) {
+				if (player.getName().equals(continentArray.get(i).getTerritory(userInput).getOwner())) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 	
 	public void beginBattle(Territory defendingTerr, Territory attackingTerr)
 	{
@@ -130,11 +217,6 @@ public class Turns {
 		}
 		
 	}
-
-	
-	
-	
-	
 	
 	public void attackTerritory(String territoryName, Continent currContinent)
 	{
--- src/main/java/JCK/Risk/Players/Player.java
+++ src/main/java/JCK/Risk/Players/Player.java
@@ -1,5 +1,6 @@
 package JCK.Risk.Players;
 
+import java.util.List;
 import java.util.ArrayList;
 
 
@@ -8,25 +9,64 @@ public class Player {
 	public String name;
 	public int rollValue;
 	public Integer turnValue;
-	
+	private int soldiersToPlace = 0;
+	private List<String> listOfTerritoriesOwned;
+	private List<String> continentsOwned;
 	ArrayList<Integer> listOfCards = new ArrayList<Integer>();
 	
+	public List<String> getTerritoriesOwned() {
+		return listOfTerritoriesOwned;
+	}
+
+	public void addTerritoryOwned(String territory) {
+		listOfTerritoriesOwned.add(territory);
+	}
+
+	public void removeTerritoryOwned(String territory) {
+		listOfTerritoriesOwned.remove(territory);
+	}
+
+	public int getSoldiersToPlace() {
+		return soldiersToPlace;
+	}
+
+	public void setSoldiersToPlace(int soldiersToPlace) {
+		this.soldiersToPlace = soldiersToPlace;
+	}
+
 	public String getName() {
 		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 	
 	public Integer getTurnValue() {
 		return turnValue;
 	}
 	
-	public void createPlayer(String playerName, Integer turnRollValue)
-	{
+	public void setTurnValue(Integer turnValue) {
+		this.turnValue = turnValue;
+	}
+
+	public List<String> getContinentsOwned() {
+		return continentsOwned;
+	}
+
+	public void addContinentOwned(String continent) {
+		continentsOwned.add(continent);
+	}
+
+	public void removeContinentOwned(String continent) {
+		continentsOwned.remove(continent);
+	}
+
+	public void createPlayer(String playerName, Integer turnRollValue) {
 		this.name = playerName;
 		this.turnValue = turnRollValue;
-		
+		listOfTerritoriesOwned = new ArrayList<String>();
 		
 	}
-	
-	
-	
 }
+
