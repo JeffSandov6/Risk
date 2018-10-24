43a7e5589b9ffa6347b4d31ab2ce5ce6fba89bcb
KayDabs <kwakukdd4@hotmail.com>
2018-10-24 00:02:12 -0500

    Amazon s3

--- src/main/java/JCK/Risk/Start.java
+++ src/main/java/JCK/Risk/Start.java
@@ -13,54 +13,11 @@ import JCK.Risk.Gameplay.Turns;
 import JCK.Risk.Locations.Continent;
 import JCK.Risk.Locations.Territory;
 import JCK.Risk.Players.Player;
-
 public class Start {
 
 	public static void main(String[] args) throws IOException {
 		
-		
-//		Player player = new Player();
-//		player.name = "Jeff";
-//		
-//		ArrayList<String> cards1 = new ArrayList<String>();
-//		
-//		cards1.add("a");
-//		cards1.add("b");
-//		cards1.add("c");
-//		cards1.add("a");
-//
-//
-//		
-//		player.listOfCards = cards1;
-//		
-//		Collections.sort(cards1);
-		
-//		Card tempCard = new Card();
 
-		
-//		int newUnits = tempCard.checkCards(player);
-//		
-//		System.out.println(cards1);
-//		
-//		System.out.println(player.listOfCards);
-
-//		
-//		int temp = 1;
-//		
-//		while(temp < 10)
-//		{
-//			System.out.print("On set " + temp + " you gain ");
-//			System.out.println(tempCard.getNextSetValue() + "units ");
-//			tempCard.setsTurnedIn++;
-//			
-//			temp++;
-//		}
-//		
-//		
-//		
-		
-		
-		
 		System.out.println("Hello, and welcome to Risk!");
 		System.out.println("How many players will be playing today?");
 		
