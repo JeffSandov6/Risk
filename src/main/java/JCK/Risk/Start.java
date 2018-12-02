6639720105471463c00b3a8e0170fb202d2ee1a2
KayDabs <kwakukdd4@hotmail.com>
2018-12-01 20:25:25 -0600

    twitter

--- src/main/java/JCK/Risk/Start.java
+++ src/main/java/JCK/Risk/Start.java
@@ -10,11 +10,43 @@ import JCK.Risk.Gameplay.Card;
 import JCK.Risk.Gameplay.Game;
 import JCK.Risk.Gameplay.Turns;
 
-public class Start {
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+
+public class Start {	
+	
+	static String consumerKeyStr = "VZe14TMsVqwjhH6qVmWAAT9x8";
+	static String consumerSecretStr = "0JWgAeWTL4A2aYSkG3iILe7xnCXHfThlCSmTlp3NRoz9sAnllH";
+	static String accessTokenStr = "727691720939298816-WbCZKP32aWY1me35bseLJLBji1sq2aK";
+	static String accessTokenSecretStr = "r1dHS7dVlQ0NF4rLQCluGMlza0zRO2GVmTqToRA2EZ2g0";
+	
+	
 	@CoverageIgnore
-	public static void main(String[] args) throws InterruptedException, TelegramApiException, IOException {
+	
+	public static void main(String[] args) //throws IOException, InterruptedException, TelegramApiException {
 		
+	{
+		try {
+			Twitter twitter = new TwitterFactory().getInstance();
+
+			twitter.setOAuthConsumer(consumerKeyStr, consumerSecretStr);
+			AccessToken accessToken = new AccessToken(accessTokenStr,
+					accessTokenSecretStr);
+
+			twitter.setOAuthAccessToken(accessToken);
+
+			twitter.updateStatus("twitter dev");
+
+			System.out.println("Successfully updated the status in Twitter.");
+		} catch (TwitterException te) {
+			te.printStackTrace();
+		}
+	}
+}
 		
+		/*
 		ApiContextInitializer.init();
 		TelegramBotsApi botsApi = new TelegramBotsApi();
 		botsApi.registerBot(new TelegramGameBot());
@@ -32,6 +64,9 @@ public class Start {
 		// initialize the players as well as continents
 		startGame.initializeGame(numPlayers, bot);
 		startGame.initializeContinents();
+        
+
+		
 		
 		cards.initializeCards(bot);
 		
@@ -42,5 +77,5 @@ public class Start {
 		
 		
 	}
+*/
 
-}
